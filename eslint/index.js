38b8096feb4304c831f4a443e7308fddf8b7bba2
vwsode <frscode1@gmail.com>
2024-09-08 21:27:27 +0500

    feat: add husky precommit hook

--- eslint/index.js
+++ eslint/index.js
@@ -1,62 +1,69 @@
 module.exports = {
-  root: true,
+    root: true,
 
-  env: {
-    browser: true,
-    es6: true,
-    es2021: true,
-    node: true,
-  },
-
-  overrides: [
-    {
-      env: { node: true },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: { sourceType: 'script' },
+    env: {
+        browser: true,
+        es6: true,
+        es2021: true,
+        node: true,
     },
-  ],
 
-  plugins: ['import'],
+    overrides: [
+        {
+            env: { node: true },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: { sourceType: 'script' },
+        },
+    ],
 
-  rules: {
-    indent: ['error', 4, { SwitchCase: 1 }],
-    'max-len': ['error', { code: 80, ignoreComments: true }],
-    'import/newline-after-import': ['error', { count: 1 }],
-    'import/order': [
-      'error',
-      {
-        alphabetize: { order: 'asc', caseInsensitive: true },
-        groups: ['type', ['builtin', 'external'], 'internal', ['sibling', 'parent'], 'index', 'unknown'],
-        'newlines-between': 'always',
-      },
-    ],
-    'array-bracket-spacing': ['error', 'never'],
-    'object-curly-spacing': ['error', 'always'],
-    'sort-imports': [
-      'error',
-      {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        allowSeparatedGroups: false,
-      },
-    ],
-    'spaced-comment': [
-      'error',
-      'always',
-      {
-        line: {
-          markers: ['/'],
-          exceptions: ['-', '+'],
-        },
-        block: {
-          markers: ['!'],
-          exceptions: ['*'],
-          balanced: true,
-        },
-      },
-    ],
-    'import/no-duplicates': ['error', { considerQueryString: true }],
-  },
+    plugins: ['import'],
+
+    rules: {
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'max-len': ['error', { code: 80, ignoreComments: true }],
+        'import/newline-after-import': ['error', { count: 1 }],
+        'import/order': [
+            'error',
+            {
+                alphabetize: { order: 'asc', caseInsensitive: true },
+                groups: [
+                    'type',
+                    ['builtin', 'external'],
+                    'internal',
+                    ['sibling', 'parent'],
+                    'index',
+                    'unknown',
+                ],
+                'newlines-between': 'always',
+            },
+        ],
+        'array-bracket-spacing': ['error', 'never'],
+        'object-curly-spacing': ['error', 'always'],
+        'sort-imports': [
+            'error',
+            {
+                ignoreCase: true,
+                ignoreDeclarationSort: true,
+                ignoreMemberSort: false,
+                memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+                allowSeparatedGroups: false,
+            },
+        ],
+        'spaced-comment': [
+            'error',
+            'always',
+            {
+                line: {
+                    markers: ['/'],
+                    exceptions: ['-', '+'],
+                },
+                block: {
+                    markers: ['!'],
+                    exceptions: ['*'],
+                    balanced: true,
+                },
+            },
+        ],
+        'import/no-duplicates': ['error', { considerQueryString: true }],
+    },
 };
--- semantic-release/index.js
+++ semantic-release/index.js
@@ -1,9 +1,9 @@
 module.exports = {
-  branches: ['master', 'main'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/git',
-  ],
+    branches: ['master', 'main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/npm',
+        '@semantic-release/git',
+    ],
 };
